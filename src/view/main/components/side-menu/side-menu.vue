9c4e6c21470b769d93014471056da57620e2e692
HejinYo <hejinyo@gmail.com>
2018-08-18 12:43:11 +0800

    bug fix : 左侧菜单加载完成后渲染，增加切换动画

--- src/view/main/components/side-menu/side-menu.vue
+++ src/view/main/components/side-menu/side-menu.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="side-menu-wrapper">
     <slot></slot>
-    <Menu ref="menu" v-show="!collapsed" :active-name="activeName" :open-names="openedNames" :accordion="accordion" :theme="theme" width="auto" @on-select="handleSelect">
-      <template v-for="item in menuList">
-        <template v-if="item.children && item.children.length === 1">
-          <side-menu-item v-if="showChildren(item)" :key="`menu-${item.name}`" :parent-item="item"></side-menu-item>
-          <menu-item v-else :name="getNameOrHref(item, true)" :key="`menu-${item.children[0].name}`">
-            <common-icon :type="item.children[0].icon || ''"/>
-            <span>{{ showTitle(item.children[0]) }}</span></menu-item>
+
+    <transition name="slide-fade">
+      <Menu ref="menu" v-show="!collapsed" :active-name="activeName" :open-names="openedNames" :accordion="accordion" :theme="theme" width="auto" @on-select="handleSelect">
+        <template v-for="item in menuList">
+          <template v-if="item.children && item.children.length === 1">
+            <side-menu-item v-if="showChildren(item)" :key="`menu-${item.name}`" :parent-item="item"></side-menu-item>
+            <menu-item v-else :name="getNameOrHref(item, true)" :key="`menu-${item.children[0].name}`">
+              <common-icon :type="item.children[0].icon || ''"/>
+              <span>{{ showTitle(item.children[0]) }}</span></menu-item>
+          </template>
+          <template v-else>
+            <side-menu-item v-if="showChildren(item)" :key="`menu-${item.name}`" :parent-item="item"></side-menu-item>
+            <menu-item v-else :name="getNameOrHref(item)" :key="`menu-${item.name}`">
+              <common-icon :type="item.icon || ''"/>
+              <span>{{ showTitle(item) }}</span></menu-item>
+          </template>
         </template>
-        <template v-else>
-          <side-menu-item v-if="showChildren(item)" :key="`menu-${item.name}`" :parent-item="item"></side-menu-item>
-          <menu-item v-else :name="getNameOrHref(item)" :key="`menu-${item.name}`">
-            <common-icon :type="item.icon || ''"/>
-            <span>{{ showTitle(item) }}</span></menu-item>
-        </template>
-      </template>
-    </Menu>
+      </Menu>
+    </transition>
+    <transition name="slide-fade-out">
     <div class="menu-collapsed" v-show="collapsed" :list="menuList">
       <template v-for="item in menuList">
         <collapsed-menu v-if="item.children && item.children.length > 1" @on-click="handleSelect" hide-title :root-icon-size="rootIconSize" :icon-size="iconSize" :theme="theme" :parent-item="item"
@@ -28,6 +32,7 @@
         </Tooltip>
       </template>
     </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -118,4 +123,28 @@
 </script>
 <style lang="less">
   @import './side-menu.less';
+  /* 可以设置不同的进入和离开动画 */
+  /* 设置持续时间和动画函数 */
+  .slide-fade-enter-active {
+    transition: all .6s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+
+  /* .slide-fade-leave-active {
+     transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+   }*/
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+
+  /* 可以设置不同的进入和离开动画 */
+  /* 设置持续时间和动画函数 */
+  .slide-fade-out-enter-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-out-enter, .slide-out-fade-leave-to
+  /* .slide-fade-leave-active for below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+  }
 </style>
--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout style="height: 100%" class="main">
     <Sider hide-trigger collapsible :width="210" :collapsed-width="64" v-model="collapsed" class="left-sider" :style="{overflow: 'hidden'}">
-      <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
+      <!-- 菜单数据加载完才开始渲染 -->
+      <side-menu  v-if="menuList.length > 0"  accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
         <div class="logo-con">
           <img v-show="!collapsed" :src="maxLogo" key="max-logo"/>
